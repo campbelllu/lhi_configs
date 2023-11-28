7fc0e710c2d5769dbdc21c7a4b6771b14d90847d
Luke <campbelllu3@gmail.com>
2023-11-27 21:45:48 -0700

    Advanced my TF knowledge. EC2's next!

--- main.tf
+++ main.tf
@@ -1,11 +1,71 @@
 #need vpc, LB, ec2's, eks, rds?
 #vpc
 resource "aws_vpc" "dev" {
-  cidr_block = "10.100.0.0/20"
+  cidr_block           = "10.100.0.0/20"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.dev.id
+  cidr_block              = "10.100.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-2a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev_public_rt" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dev_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+}
+
+resource "aws_route_table_association" "dev_public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+
+}
+
+resource "aws_security_group" "dev_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.dev.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #What can get into subnet? Only for demonstration purposes! Always only allow specifics for ingress!!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #Where can subnet get to?
+  }
+
 }
--- providers.tf
+++ providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.26.0" 
+      version = "~> 5.26.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  region                   = "us-east-2"
   shared_credentials_files = ["~/.aws/credentials"]
-  profile = "terraformer"
+  profile                  = "terraformer"
 }
